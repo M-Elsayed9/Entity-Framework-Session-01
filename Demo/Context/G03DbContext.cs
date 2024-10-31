2fe6bf419df350d4593a4c637df674379654c4d5
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-10-31 05:56:52 +0300

    Ways of Mapping (By fluent API and Configurations)

--- Demo/Context/G03DbContext.cs
+++ Demo/Context/G03DbContext.cs
@@ -1,4 +1,5 @@
-﻿using Demo.Entities;
+﻿using Demo.Configurations;
+using Demo.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,49 @@ namespace Demo.Context
         {
             optionsBuilder.UseSqlServer("Server = MOHAMED_PC; Database = CompanyG03DB; Trusted_Connection = true; TrustServerCertificate = True;");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Employee>(new EmployeeConfiguration());
+
+            //modelBuilder.ApplyConfigurationsFromAssembly(modelBuilder.GetType().Assembly);
+            //modelBuilder.Entity<Employee>().HasKey(e => e.EmpId);
+
+            //modelBuilder.Entity<Employee>().Property(e => e.EmpId /*or "EmpId" or nameof(Employee.EmpId) */).UseIdentityColumn(1, 1);
+
+            //modelBuilder.Entity<Employee>().Property(e => e.EmpName).HasColumnType("varchar").HasMaxLength(50).HasDefaultValue("EMpl").IsRequired();
+
+            //modelBuilder.Entity<Employee>().Property(e => e.Salary).HasColumnType("Money");
+
+            //modelBuilder.Entity<Employee>().Property(e => e.Age).HasColumnType("int");
+
+            //modelBuilder.Entity<Employee>().Property(e => e.Email).HasColumnType("varchar").HasMaxLength(50);
+
+            //modelBuilder.Entity<Employee>().Property(e => e.Address).HasMaxLength(50);
+
+            //modelBuilder.Entity<Employee>(e =>
+            //{
+            //    e.HasKey(e => e.EmpId);
+
+            //    e.Property(e => e.EmpId /*or "EmpId" or nameof(Employee.EmpId) */).UseIdentityColumn(1, 1);
+
+            //    e.Property(e => e.EmpName).HasColumnType("varchar").HasMaxLength(50).HasDefaultValue("EMpl").IsRequired();
+
+            //    e.Property(e => e.Salary).HasColumnType("Money");
+
+            //    e.Property(e => e.Age).HasColumnType("int");
+
+            //    e.Property(e => e.Email).HasColumnType("varchar").HasMaxLength(50);
+
+            //    e.Property(e => e.Address).HasMaxLength(50);
+            //});
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
